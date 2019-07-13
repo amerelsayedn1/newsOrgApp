05e38a610ba61cff5f271e5c6fc5f4a4d03805ef
Amer Elsayed <dev.amer.elsayed@gmail.com>
2019-07-13 10:25:03 -0700

    Remove unused dependcies

--- app/src/main/java/com/alpha/neworg/utilites/BindingUtils.kt
+++ app/src/main/java/com/alpha/neworg/utilites/BindingUtils.kt
@@ -8,33 +8,6 @@ import com.bumptech.glide.Glide
 
 object BindingUtils {
 
-    /* fun convertToSuffix(count: Long): String {
-         if (count < 1000) return "" + count
-         val exp = (Math.log(count.toDouble()) / Math.log(1000.0)).toInt()
-         return String.format(
-             "%.1f%c",
-             count / Math.pow(1000.0, exp.toDouble()),
-             "kmgtpe"[exp - 1]
-         )
-     }*/
-
-    /*@BindingAdapter("imageurl")
-    @JvmStatic
-    fun setImageByRes(view: ImageView, imageUrl: String) {
-        Glide.with(view.context).load("http://tepci.xyz/public/$imageUrl").placeholder(R.mipmap.tepci_logo).into(view)
-        Log.e("TTTT",imageUrl+"Empty_Image")
-    }*/
-
-    /*@BindingAdapter(value = ["android:src"], requireAll = false)
-    @JvmStatic
-    fun loadImage(view: ImageView, url: String) {
-        if (!TextUtils.isEmpty(url)) {
-            Glide.with(view.context).load("http://tepci.xyz/public/$url").placeholder(R.mipmap.tepci_green_logo).into(view)
-        }
-
-    }*/
-
-
     @BindingAdapter("bind:src")
     @JvmStatic
     fun setimgresource(view: ImageView, url: String) {
--- app/src/main/java/com/alpha/neworg/utilites/CommonUtils.kt
+++ app/src/main/java/com/alpha/neworg/utilites/CommonUtils.kt
@@ -5,26 +5,11 @@ package com.alpha.neworg.utilites
 import android.content.Context
 import android.net.ConnectivityManager
 import android.os.Build
-import android.util.Log
 import android.view.*
-import android.view.animation.AnimationUtils
 import android.widget.TextView
-import androidx.annotation.RequiresApi
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
-import java.util.*
 import android.net.NetworkCapabilities
-
-
-
-
-
-
-
-
 
 
 /**
@@ -34,29 +19,7 @@ import android.net.NetworkCapabilities
 @Suppress("SENSELESS_COMPARISON")
 object CommonUtils {
 
-
-
-    /*Recycle Animation*/
-    fun runReycleAnimation(recycleView: RecyclerView, anim_res: Int) {
-        val content = recycleView.context
-        val layoutAnimationController = AnimationUtils.loadLayoutAnimation(content, anim_res)
-        recycleView.layoutAnimation = layoutAnimationController
-        recycleView.adapter!!.notifyDataSetChanged()
-        recycleView.scheduleLayoutAnimation()
-    }
-
-    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
-    fun setLocale(old: Context, locale: Locale): Context {
-        val oldConfig = old.resources.configuration
-        oldConfig.setLocale(locale)
-        return old.createConfigurationContext(oldConfig)
-    }
-
-
-
     /*To check Validation*/
-
-
 
     @Suppress("DEPRECATION")
     fun isNetworkConnected(context: Context): Boolean {
@@ -82,100 +45,6 @@ object CommonUtils {
         tvMsg.typeface = typeface
         snackBar.show()
     }
-
-
-    /*ُEmail Error Message*/
-    fun isEmailValid(email: CharSequence): Boolean {
-        return !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
-    }
-
-
-    /*To check if fragment is visible*/
-    fun isVisible(context: AppCompatActivity, fragment_container: Int, fragment_tag: String, fragment: Fragment) {
-        val fragment_main = context.supportFragmentManager.findFragmentByTag(fragment_tag)
-        if (fragment_main != null && fragment_main.isVisible) {
-            Log.e("Fragment_is_visible", "Visible")
-        } else {
-            clearStack(context)
-            context.supportFragmentManager.beginTransaction().replace(fragment_container, fragment, fragment_tag)
-                .commit()
-        }
-    }
-
-
-    /*To Clear Back Stack*/
-    fun clearStack(context: AppCompatActivity) {
-        val backStackEntry = context.supportFragmentManager.backStackEntryCount
-        if (backStackEntry > 0) {
-            for (i in 0 until backStackEntry)
-                context.supportFragmentManager.popBackStackImmediate()
-        }
-
-        if (context.supportFragmentManager.fragments != null && context.supportFragmentManager.fragments.size > 0) {
-            for (i in 0 until context.supportFragmentManager.fragments.size) {
-                val fragment = context.supportFragmentManager.fragments[i]
-                if (fragment != null)
-                    context.supportFragmentManager.beginTransaction().remove(fragment).commit()
-            }
-        }
-    }
-
-
-
-    /*fun isWiFiConnected(context: Context): Boolean {
-        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
-        if (cm != null) {
-            if (Build.VERSION.SDK_INT < 23) {
-                val ni = cm.activeNetworkInfo
-
-                if (ni != null) {
-                    return ni.isConnected && (ni.type == ConnectivityManager.TYPE_WIFI || ni.type == ConnectivityManager.TYPE_MOBILE)
-                }
-            } else {
-                val n = cm.activeNetwork
-
-                if (n != null) {
-                    val nc = cm.getNetworkCapabilities(n)
-
-                    return nc.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || nc.hasTransport(
-                        NetworkCapabilities.TRANSPORT_WIFI
-                    )
-                }
-            }
-        }
-
-        return false
-    }
-
-
-    fun isNetworkConnected(context: Context): Boolean {
-        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
-        if (cm != null) {
-            if (Build.VERSION.SDK_INT < 23) {
-                val ni = cm.activeNetworkInfo
-
-                if (ni != null) {
-                    return ni.isConnected && (ni.type == ConnectivityManager.TYPE_WIFI || ni.type == ConnectivityManager.TYPE_MOBILE)
-                }
-            } else {
-                val n = cm.activeNetwork
-
-                if (n != null) {
-                    val nc = cm.getNetworkCapabilities(n)
-
-                    return nc.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || nc.hasTransport(
-                        NetworkCapabilities.TRANSPORT_WIFI
-                    )
-                }
-            }
-        }
-
-        return false
-    }*/
-
-
 
 
 }
